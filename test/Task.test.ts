b542f609e1b0b13ef8146171715464e8f3a26d0f
Max Ershov <maxx95.95@gmail.com>
2021-03-28 23:42:41 +0300

    fix: Task test

--- test/Task.test.ts
+++ test/Task.test.ts
@@ -2,7 +2,7 @@ import { Task } from '../src/Classes/Task';
 
 describe('Task test', () => {
   it('should set title', () => {
-    const task = new Task('test');
+    const task = new Task('test', 100);
     return expect(task.title).toEqual('test');
   });
 });
