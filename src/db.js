660062644a909324bd0fd756d8c4f9820844fe4a
Max Ershov <maxx95.95@gmail.com>
2021-06-21 22:42:58 +0300

    feat: postgres db

---
+++ src/db.js
@@ -0,0 +1,19 @@
+require('dotenv').config({ path: '../.env' })
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+})
+
+console.log(process.env.DB_USER);
+
+pool.query('SELECT * FROM public."TASKS"', (error, results) => {
+    if (error) {
+        throw error
+    }
+    console.log(results.rows);
+})
