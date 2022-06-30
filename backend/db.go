8045d39756330cae7c3f7cf71ca92e5f25a51e8c
Max Ershov <maxx95.95@gmail.com>
2022-07-01 01:02:35 +0300

    refactor: AddTask

--- backend/db.go
+++ backend/db.go
@@ -37,10 +37,21 @@ func ReadData() []Task {
 }
 
 func AddTask() {
+	task1 := Task{
+		Date:   "2021-06-19T21:00:00.000Z",
+		ID:     3,
+		Type:   "TASK",
+		UserId: 1,
+		Title:  "Third Task",
+		Price:  700}
 	db, err := sql.Open("sqlite3", "data.db")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-	db.Exec("INSERT INTO Tasks (ID, Date, Type, UserId, Price, Title) VALUES ($1,$2,$3,$4,$5, $6)", 3, "2021-06-19T21:00:00.000Z", "TASK", 1, 600, "Third")
+	result, err := db.Exec("INSERT INTO Tasks (ID, Date, Type, UserId, Price, Title) VALUES ($1,$2,$3,$4,$5, $6)", task1.ID, task1.Date, task1.Type, task1.UserId, task1.Price, task1.Title)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
 }
